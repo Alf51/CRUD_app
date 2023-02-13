4d5ea20ec546d031266cb5be031907e7bfcce83d
Vasiliy <zakupm51@gmail.com>
2023-02-13 21:36:22 +0300

    add controller and page: -hello -goodbye

--- src/main/java/org/goldenalf/springcourse/controlers/FirstControler.java
+++ src/main/java/org/goldenalf/springcourse/controlers/FirstControler.java
@@ -2,13 +2,24 @@ package org.goldenalf.springcourse.controlers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstControler {
 
     @GetMapping("/hello")
-    public String sayHellow() {
+    public String sayHellow(@RequestParam(value = "name", required = false) String name) {
+        if (name != null && name.equals("vasily")) {
+            return "first/secredPage";
+        }
         return "first/hello";
+    }
+
+    @GetMapping("/index")
+    public String getIndexPage() {
+        return "UBEREats/index";
     }
 
     @GetMapping("/goodbye")
