fe37dfbc061926fed58177ea1178661c6a9be183
Vasiliy <zakupm51@gmail.com>
2023-02-16 14:45:23 +0300

    added: -PeopleController

--- src/main/java/org/goldenalf/springcourse/controlers/PeopleController.java
+++ src/main/java/org/goldenalf/springcourse/controlers/PeopleController.java
@@ -1,35 +1,35 @@
-//package org.goldenalf.springcourse.controlers;
-//
-//import org.goldenalf.springcourse.dao.PeopleDAO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/people")
-//public class PeopleController {
-//    private final PeopleDAO peopleDAO;
-//
-//    @Autowired
-//    public PeopleController(PeopleDAO peopleDAO) {
-//        this.peopleDAO = peopleDAO;
-//    }
-//
-//    @GetMapping()
-//    public String index(Model model) {
-//        //dao возвращает список всех людей
-//        model.addAttribute("people", peopleDAO.index());
-//        return "people/index";
-//    }
-//
-//    @GetMapping("/{id}")
-//    public String show(@PathVariable("id") int id, Model model) {
-//        //dao возвращает список всех людей
-//        System.out.println(peopleDAO.show(id).getName());
-//        model.addAttribute("person", peopleDAO.show(id));
-//        return "people/show";
-//    }
-//}
+package org.goldenalf.springcourse.controlers;
+
+import org.goldenalf.springcourse.dao.PeopleDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    private final PeopleDAO peopleDAO;
+
+    @Autowired
+    public PeopleController(PeopleDAO peopleDAO) {
+        this.peopleDAO = peopleDAO;
+    }
+
+    @GetMapping({"/", ""})
+    public String index(Model model) {
+        //dao возвращает список всех людей
+        model.addAttribute("people", peopleDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        //dao возвращает список всех людей
+        System.out.println(peopleDAO.show(id).getName());
+        model.addAttribute("person", peopleDAO.show(id).getName());
+        return "people/show";
+    }
+}
